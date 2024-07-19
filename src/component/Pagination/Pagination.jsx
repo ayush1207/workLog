d548ba2cac3384dd343fb3f9f8f6fd9bc8e00b59
ayush <ayush@reachme.com>
2024-07-19 10:14:56 +0530

    chore : updated pagination

--- src/component/Pagination/Pagination.jsx
+++ src/component/Pagination/Pagination.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export default function Pagination({pageNumber,setPageNumber}){
+    let page;
+    let List = [];
+    useEffect(()=>{
+        page = {
+            prev : (pageNumber - 3  > 0) ? pageNumber - 3 :  1,
+            next : pageNumber + 3
+        }
+
+        for(let i=Math.min(page.prev,pageNumber);i<=page.next;i++){
+            List.push(i);
+        }
+    },[pageNumber]); 
 
     function next(){
         setPageNumber((prevPage)=>prevPage+1);
@@ -10,7 +24,11 @@ export default function Pagination({pageNumber,setPageNumber}){
     return (
         <divc className="pagination-container">
             <div onClick={prev}>{"<"}</div>
-            <div>{pageNumber}</div>
+            {
+                List.forEach(data => {
+                    return <div className={data == pageNumber ? 'active' : ''}>{data}</div>
+                })
+            }
             <div onClick={next}>{">"}</div>
         </divc>
     )
